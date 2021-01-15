17ce9aede8be76b45edb2c19bcf7c865431d0da8
Lucas Sesboüé <lucas.sesboue@gmail.com>
2021-01-15 17:22:43 +0100

    feat: PLAYER WORKS

--- client/src/components/Player/Player.tsx
+++ client/src/components/Player/Player.tsx
@@ -1,4 +1,3 @@
-import { useContext } from 'react';
 import { ReleaseInterface } from '../../interfaces';
 import './Player.scss';
 
@@ -10,16 +9,23 @@ export interface PlayerProps {
 }
 
 const Player: React.FC<PlayerProps> = ({ nowPlaying }) => {
-  const { url, artist, title, bandcampId } = nowPlaying.current;
+  const { url, artist, title, bandcampId, bandcampAlbumId, itemType } = nowPlaying.current;
+
+  const param1 = itemType;
+  const param2 = bandcampAlbumId && bandcampAlbumId !== bandcampId ? `=${bandcampAlbumId}/` : `=${bandcampId}/`;
+  const param3 = bandcampAlbumId && bandcampAlbumId !== bandcampId && `track=${bandcampId}/`;
 
   const src =
-    'https://bandcamp.com/EmbeddedPlayer/album=' +
-    bandcampId +
-    '/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/';
+    'https://bandcamp.com/EmbeddedPlayer/' +
+    param1 +
+    param2 +
+    '/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/' +
+    param3 +
+    'transparent=true/';
   return (
     <div id='player'>
       <h1>Ich bin das Player</h1>
-      <iframe src={src} seamless>
+      <iframe src={src} seamless title='bc-player'>
         <a href={url}>
           {title} by {artist}
         </a>
@@ -30,8 +36,7 @@ const Player: React.FC<PlayerProps> = ({ nowPlaying }) => {
 
 export default Player;
 
-{
-  /* 
+/* 
 Format when it's a track from an album
 https://bandcamp.com/EmbeddedPlayer/album=95932365/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/track=1855288359/transparent=true/
 
@@ -41,4 +46,3 @@ https://bandcamp.com/EmbeddedPlayer/album=95932365/size=large/bgcol=ffffff/linkc
 Format when it's a single track
 https://bandcamp.com/EmbeddedPlayer/track=1788183451/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/
 */
-}
